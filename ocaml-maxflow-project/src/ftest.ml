42d51c97804e0a21886aa20208dc482033eb1d1c
OthmaneSL <95209283+OthmaneSL@users.noreply.github.com>
2023-01-11 18:07:19 +0100

    ajout des phases de test

--- ocaml-maxflow-project/src/ftest.ml
+++ ocaml-maxflow-project/src/ftest.ml
@@ -28,47 +28,47 @@ let () =
   in
 
   (* Open file *)
-  let graph = from_file infile in
+
+  (*test de construire res*************************************)
+  (*let graph = from_file infile in
   let graph = gmap graph int_of_string in 
-  (*let () = export outfile graph in *)
-  (*let int_of_intoption = function None -> [] | Some n -> n in 
-    let chemin = trouver_chemin graph 0 5 in 
-    let rec print_tuples =
-    function
-    | [] -> ()
-    | (a, b) :: rest ->
-      Printf.printf "(%i, %i); " a b;
-      print_tuples rest in 
+  let graph = int_to_flot graph in 
+  let graph = construire_res graph in 
+  let graph = gmap graph string_of_int in
+  let () = export outfile graph in
+  ()*)
+(************************************************************)
 
-    let () =
-    print_tuples (int_of_intoption chemin) in 
-    ()*)
+(*********   test de flot update ****************************)
+  (*let graph = from_file infile in
+  let graph = gmap graph int_of_string in 
+  let graph = int_to_flot graph in 
+  let graph = flot_update [(0,1)] 2 in 
+  let graph = flot_to_string graph in 
+  let () = export outfile graph in
+  ()*)
+  (**********************************************************)
 
-
-  (*let () =
-    Printf.printf "%d\n" (int_of_intoption (trouver_chemin graph 0 5))*)
-
-  (* we'd rather do something different on None *)
+(***********    test de trouver chemin *********************)
+  (*let graph = from_file infile in
+  let graph = gmap graph int_of_string in 
   let l = trouver_chemin graph 0 5 in 
   let l = transformationliste l in 
   let l= Option.get l in 
-  let ()= List.iter (fun (a,b) -> Printf.printf "(%i, %i); " a b ) l in 
-
-
-
-  (*let graph = add_arc graph 0 2 2 in *)
-  (*let graph = int_to_flot graph in *)
-  (*test flot update ok*)
-  (*let graph = flot_update graph [(4,5)] 10 in 
-    let graph = construire_res graph in*) 
-  (*let graph = gmap graph string_of_int in*)
-  (*let graph = flot_to_string graph in*) 
-
-  (*let graph = construire_res graph in 
-    let graph = gmap graph string_of_int in*)
-  let graph = gmap graph string_of_int in 
-
-  (* Rewrite the graph that has been read. *)
+  let ()= List.iter (fun (a,b) -> Printf.printf "(%i, %i); " a b ) l in
   let () = write_file outfile graph in
-  ()
+  ()*)
+(************************************************************) 
+ 
+(****************    test de run ***************************)
 
+
+  (*let graph = from_file infile in
+  let graph = gmap graph int_of_string in 
+  let graph = run graph _source _sink in 
+  let graph = flot_to_string graph in 
+  let () = export outfile graph in
+  ()*)
+
+(***********************************************************)
+  
